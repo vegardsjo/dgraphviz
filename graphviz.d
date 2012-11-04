42f6fe248f68258178d0f7870d1119cf46a6eb35
Vegard Sjonfjell <vegardsjo@gmail.com>
2012-11-05 00:24:51 +0100

    removed unit test from graphviz.d

--- graphviz.d
+++ graphviz.d
@@ -115,29 +115,3 @@ class Edge
 
   mixin(gv_attrAccess!("gv_edge"));
 }
-
-unittest
-{
-  auto graph = new Graph(GraphType.digraph);
-
-  auto node1 = graph.addNode("Node 1");
-  node1.style = "filled";
-  node1.fillcolor = "red";
-
-  auto node2 = graph.addNode("Node 2");
-  node2.label = " ♖ ";
-
-  auto edge1 = graph.addEdge(node1, node1);
-  auto edge2 = graph.addEdge(node1, node2);
-  auto edge3 = graph.addEdge(node2, node1);
-
-  auto edge4 = graph.addEdge(node2, node2);
-  edge4.color = "magenta";
-  edge4.label = "OBEY ME, FOR I AM AN EDGE";
-
-  graph.layout = "dot";
-  //graph.outputFile("png", "test.png");
-  graph.outputFp("dot", stdout);
-}
-
-version (test) void main() {}
